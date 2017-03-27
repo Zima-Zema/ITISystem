9ee722791db1ae52eef4ede350dd84ae3e8c1c86
Zima-Zema <blackflower225@gmail.com>
2017-03-27 18:54:10 +0200

    Work with DepartmentController

---
+++ ITISystem/Migrations/201703261936210_FixValidation.cs
@@ -0,0 +1,16 @@
+namespace ITISystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixValidation : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- ITISystem/Models/Student.cs
+++ ITISystem/Models/Student.cs
@@ -17,34 +17,33 @@ namespace ITISystem.Models
         }
         [Key]
         public int Student_Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="*")]
         [DataType(DataType.Text, ErrorMessage = "Please enter valid Name")]
         public string FirstName { get; set; }
         [DataType(DataType.Text, ErrorMessage = "Please enter valid Name")]
         public string LastName { get; set; }
         [DataType(DataType.Date, ErrorMessage = "Enter Valid Date")]
-        [RegularExpression("")]
         public DateTime BirthDate { get; set; }
-        [Required]
+        [Required(ErrorMessage ="*")]
         [DataType(DataType.EmailAddress, ErrorMessage = "Enter Valid Email")]
         [EmailAddress]    
         public string Email { get; set; }
         //
-        [Required]
+        [Required(ErrorMessage ="*")]
         [Index(IsUnique = true)]
         public string UserName { get; set; }
         //
-        [Required]
+        [Required(ErrorMessage ="*")]
         [DataType(DataType.Password, ErrorMessage = "Please Enter Strong Password ")]
-        [RegularExpression("[^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$]")]
+        [RegularExpression(@"[^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$]")]
         public string Password { get; set; }
         //
         [Range(0, 600, ErrorMessage = "Please enter valid Number")]
-        [Required]
+        [Required(ErrorMessage ="*")]
         public int Attend_Balance { get; set; }
         //
         //
-        [Range(7, 11, ErrorMessage = "Please enter valid Phone")]
+        [Required(ErrorMessage ="*")]
         [DataType(DataType.PhoneNumber, ErrorMessage = "Enter Valid Phone")]
         public int Telephone { get; set; }
         //
@@ -55,7 +54,7 @@ namespace ITISystem.Models
         //
         public virtual Department Department { get; set; }
         //
-        [Required]
+        [Required(ErrorMessage ="*")]
         public virtual Attendance Attendances { get; set; }
         //
         public virtual List<Std_Crs_Instr> Std_Crs_Instr { get; set; }
