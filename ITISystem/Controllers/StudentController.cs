58a0792e56c84d7f8ff78abca2ad14287319d66b
Hend <Hend@DESKTOP-CHKSNRL>
2017-03-29 18:39:59 +0200

    last-ver

--- ITISystem/Controllers/StudentController.cs
+++ ITISystem/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using ITISystem.Models;
+using ITISystem.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity.Validation;
@@ -15,14 +16,19 @@ namespace ITISystem.Controllers
         // GET: Student
         public ActionResult Index()
         {
-            ViewBag.dpts = new SelectList(iti.Departments, "Department_id", "Name");
-            var models = iti.Students;
+
+            var viewModel = new StudentViewModel()
+            {
+                DepartmentList = iti.Departments.ToList(),
+                StudentList = iti.Students.ToList()
+
+            };
             //iti.Students.Select(s=>s.Department_Key);
             //(from s in iti.Students select new Student() {FirstName=s.FirstName,LastName=s.LastName,BirthDate=s.BirthDate,Email=s.Email,UserName=s.UserName,Password=s.Password,Attend_Balance=s.Attend_Balance,Telephone=s.Telephone,Address=s.Address,Department_Key=s.Department_Key }).ToList();
             /*(from s in iti.Students
                       join d in iti.Departments on s.Department_Key equals d.Department_Id
                       select new Std_Dept() { dept_name = d.dept_name, name = s.name, age = s.age, id = s.id }).ToList();*/
-            return View(models);
+            return View(viewModel);
         }
         //[HttpGet]
         //public ActionResult Create_Student()
@@ -30,22 +36,22 @@ namespace ITISystem.Controllers
         //    ViewBag.dpts = new SelectList(iti.Departments, "Department_id", "Name");
         //    return View();
         //}
-        //[HttpPost]
-        public ActionResult Create_Student(Student std)
+        [HttpPost]
+        public ActionResult Create_Student(ViewModel.StudentViewModel std)
         {
-           
+            Student std2 = std.Student;
             //int? selected_dept= std.Department_Key;
             //string cap= iti.Departments.Where(d => d.Department_Id == selected_dept).Select(d => d.Capacity).ToString();
             //string count_std = iti.Students.Count().ToString();
 
             //if (ModelState.IsValid)
             //{
-            iti.Students.Add(std);
+            iti.Students.Add(std2);
                     try
                     {
                         iti.SaveChanges();
                 var models = iti.Students;
-                return PartialView(models);
+                return RedirectToAction("Index");
                     }
                     catch (DbEntityValidationException ex)
                     {
@@ -59,54 +65,56 @@ namespace ITISystem.Controllers
 
                         throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
                     }
-         //   }
-            //else
-            //{
-            //    ViewBag.dpts = new SelectList(iti.Departments, "Department_id", "Name");
-            //    return View(std);
-            //}
 
         }
         [HttpGet]
         public ActionResult edit(int Id)
         {
-            Student std = iti.Students.SingleOrDefault(a=>a.Student_Id==Id);
-            ViewBag.dpts = new SelectList(iti.Departments, "Department_id", "Name");
-            return PartialView(std);
+          
+
+            var viewModel2 = new StudentViewModel()
+            {
+                DepartmentList = iti.Departments.ToList(),
+                StudentList = iti.Students.ToList(),
+                Student = iti.Students.SingleOrDefault(a => a.Student_Id == Id)
+
+
+        };
+            return View(viewModel2);
         }
 
         [HttpPost]
-        public ActionResult edit(Student st)
+        public ActionResult edit(ViewModel.StudentViewModel st)
         {
-            
-            Student std_old = iti.Students.SingleOrDefault(a => a.Student_Id == st.Student_Id);
-            std_old.FirstName = st.FirstName;
-            std_old.LastName = st.LastName;
-            std_old.Password = st.Password;
-            std_old.BirthDate = st.BirthDate;
-            std_old.Email = st.Email;
-            std_old.UserName = st.UserName;
-            std_old.Attend_Balance = st.Attend_Balance;
-            std_old.Telephone = st.Telephone;
-            std_old.Address.City = st.Address.City;
-            std_old.Address.Country = st.Address.Country;
-            std_old.Address.Street = st.Address.Street;
-            std_old.Department_Key = st.Department_Key;
+
+            Student std_new = st.Student;
+            Student std_old = iti.Students.SingleOrDefault(a => a.Student_Id == std_new.Student_Id);
+            std_old.FirstName = std_new.FirstName;
+            std_old.LastName = std_new.LastName;
+            std_old.Password = std_new.Password;
+            std_old.BirthDate = std_new.BirthDate;
+            std_old.Email = std_new.Email;
+            std_old.UserName = std_new.UserName;
+            std_old.Attend_Balance = std_new.Attend_Balance;
+            std_old.Telephone = std_new.Telephone;
+            std_old.Address.City = std_new.Address.City;
+            std_old.Address.Country = std_new.Address.Country;
+            std_old.Address.Street = std_new.Address.Street;
+            std_old.Department_Key = std_new.Department_Key;
             iti.SaveChanges();
-            var models = iti.Students;
-            return PartialView("Create_Student",models);
+
+            return RedirectToAction("Index");
         }
         [HttpGet]
         public ActionResult delete(int Id)
-        {
-            
+        {  
             Student std = iti.Students.SingleOrDefault(a => a.Student_Id == Id);
             return View(std);
         }
         [HttpPost]
         public ActionResult delete(Student std)
         {
-           
+            
             iti.Students.Remove(std);
             iti.SaveChanges();
             return RedirectToAction("index");
