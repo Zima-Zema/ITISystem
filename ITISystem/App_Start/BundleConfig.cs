36c3ad3fb0fdde7bc17c72eb47a3850c7452aaa6
Zima-Zema <blackflower225@gmail.com>
2017-04-04 00:33:28 +0200

    update bundle

--- ITISystem/App_Start/BundleConfig.cs
+++ ITISystem/App_Start/BundleConfig.cs
@@ -8,10 +8,7 @@ namespace ITISystem
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js",
-            //            "~/Scripts/DataTables/jquery.dataTables.js",
-            //            "~/Scripts/DataTables/datatables.bootstrap.js"));
+    
 
 
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
